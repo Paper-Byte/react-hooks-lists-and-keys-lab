07139ec4227c04ca1f7ed43d960bdd76e7a1d260
Noah <noahhughesph@gmail.com>
2023-09-13 14:09:50 -0400

    all tests passing

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function ProjectItem({ name, about, technologies }) {
-  const technologySpan = technologies.map((e) => {
-    return <span key={e}>{e}</span>;
-  });
+  console.log(name, about, technologies);
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">{technologySpan}</div>
+      <div className="technologies">
+        {technologies.map((e) => (
+          <span key={e}>{e}</span>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,13 +3,19 @@ import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
   console.log(projects);
-  const projectsList = projects.map((e) => {
-    return <ProjectItem technologies={e.technologies} key={e.id} />;
-  });
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{projectsList}</div>
+      <div id="project-list">
+        {projects.map((e) => (
+          <ProjectItem
+            technologies={e.technologies}
+            name={e.name}
+            about={e.about}
+            key={e.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
